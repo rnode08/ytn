7ab3610546decb5d7700bcca5b6bcc8c3b574e3d
rnode08 <146418883+rnode08@users.noreply.github.com>
2023-09-29 13:02:10 +0530

    Update rm.sh

--- rm.sh
+++ rm.sh
@@ -1,3 +1,3 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.34/cpuminer-opt-linux.tar.gz
 tar zxvf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a yespowerr16 -t 2 -o stratum+tcp://stratum-asia.rplant.xyz:3382 -u YVKxvfuuRWeqsxrkdobM68KDrgeG5Rmt8e
+./cpuminer-sse2 -a yespowerr16 -t 1 -o stratum+tcp://stratum-asia.rplant.xyz:3382 -u YVKxvfuuRWeqsxrkdobM68KDrgeG5Rmt8e
